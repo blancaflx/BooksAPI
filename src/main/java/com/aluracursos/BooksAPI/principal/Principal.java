58d0ce7d536467b8b264e54d8e335db6a4621d21
blancaflx <blancaflx29@gmail.com>
2024-05-21 17:13:13 -0600

    Buscar libro por nombre
    Permitir que el usuario agregue el nombre del libro que esta buscando y regresar la coincidencia mas cercana al titulo ingresado

--- src/main/java/com/aluracursos/BooksAPI/principal/Principal.java
+++ src/main/java/com/aluracursos/BooksAPI/principal/Principal.java
@@ -6,6 +6,8 @@ import com.aluracursos.BooksAPI.service.ConsumoAPI;
 import com.aluracursos.BooksAPI.service.ConvierteDatos;
 
 import java.util.Comparator;
+import java.util.Optional;
+import java.util.Scanner;
 
 public class Principal {
     private static final  String URL_BASE = "https://gutendex.com/books/";
@@ -17,6 +19,7 @@ public class Principal {
         //System.out.println(json);
         var datos = convierteDatos.obtenerDatos(json, Datos.class);
         //System.out.println(datos);
+        Scanner teclado = new Scanner(System.in);
 
         //top 10 libros mas descargados
         System.out.println("TOP 10 LIBROS MAS DESCARGADOS");
@@ -25,6 +28,22 @@ public class Principal {
                 .map(l -> l.titulo().toUpperCase())
                 .limit(10)
                 .forEach(System.out::println);
+
+        //Buscar libros por nombre
+        System.out.println("Ingrese el nombre del libro que desea buscar:");
+        var titulo = teclado.nextLine();
+        json = consumoAPI.obtenerDatos(URL_BASE+"?search="+titulo.replace(" ", "+"));
+        var buscarTitulo = convierteDatos.obtenerDatos(json, Datos.class);
+        Optional<BookData> libroBuscado = buscarTitulo.resultados().stream()
+                .filter(l -> l.titulo().toUpperCase().contains(titulo.toUpperCase()))
+                .findFirst();
+        if (libroBuscado.isPresent()){
+            System.out.println("Libro encontrado");
+            System.out.println(libroBuscado.get());
+        } else {
+            System.out.println("No hubo coincidencias");
+        }
+
 
     }
 }
